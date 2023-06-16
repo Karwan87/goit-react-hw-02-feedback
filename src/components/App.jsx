d65d75f38d2a02bf0c79c09350af66c83525d4c6
Karwan87 <damianuszynski@gmail.com>
2023-06-16 20:24:24 +0200

    Code refactoring
    Refactor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,31 @@
+import React, { useState } from 'react';
+import styles from './var.module.css';
 import Feedback from './Feedback/Feedback';
+import Statistics from './Statistics/Statistics';
+import Section from './Section/Section';
 
 export const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleFeedback = type => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [type]: prevFeedback[type] + 1,
+    }));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <Feedback />
+    <div className={styles.feedbackContainer}>
+      <div className={styles.feedbackWindow}>
+        <Section title="Please leave feedback" />
+        <Feedback onLeaveFeedback={handleFeedback} />
+        <Section title="Statistics" />
+        <Statistics feedback={feedback} />
+      </div>
     </div>
   );
 };
